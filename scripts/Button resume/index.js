274cf6aa2feacf40facf3e7bc6d99a8c19193c1c
Tiago Farias da Silva <tfarias059@gmail.com>
2021-06-11 22:41:39 -0300

    Arrumei o scroll do detalhes

--- scripts/Button resume/index.js
+++ scripts/Button resume/index.js
@@ -1,14 +1,16 @@
-let resume = document.querySelector('.resume');
 let button = document.querySelector('.verMais');
 let ellipsis = document.querySelector('#ellipsis');
 let visible = document.querySelector('.moreText');
 
 function verMais() {
-    if (visible.style.display == "none") {
+    if (visible.style.display == 'none') {
         visible.style.display = "inline-block";
-        button.style.display = "none"
+        button.style.display = "none";
+        ellipsis.style.display = "none";
+
     } else {
-        visible.style.display = "none"
-        button.style.display = "inline-block"
+        visible.style.display = "none";
+        button.style.display = "inline-block";
+        ellipsis.style.display = "inline";
     }
 }
