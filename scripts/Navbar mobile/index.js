d43dfb61aa0e03c7c244fd3f49ce5769a0194dab
Tiago Farias da Silva <tfarias059@gmail.com>
2021-06-14 18:16:08 -0300

    Arrumei o bug do botão newTopic ficar sobreposto ao navbar em dispositvos mobile

--- scripts/Navbar mobile/index.js
+++ scripts/Navbar mobile/index.js
@@ -1,27 +1,28 @@
 let navbar = document.querySelector('#nav-list');
 let backgroundBlack = document.querySelector('.background-black');
 let body = document.querySelector('.body')
+let buttonNewTopic = document.querySelector('.button-newTopic')
 
 onload = function() {
     navbar.style.left = '-100%';
 }
 
 function checked() {
+    navbar.style.transform = 'translateY(-2.2%)';
 
-    navbar.style.transform = "translateY(-2.2%)";
-    console.log(navbar.style.transform)
-
-    if (navbar.style.left == "-100%" ) {
-        navbar.style.left = "0";
+    if (navbar.style.left == '-100%' ) {
+        navbar.style.left = '0';
         backgroundBlack.style.display = 'block';
+        buttonNewTopic.style.zIndex = "-1";
 
     } else {
-        navbar.style.left = "-100%";
-        backgroundBlack.style.display = 'none'
+        navbar.style.left = '-100%';
+        backgroundBlack.style.display = 'none';
+        buttonNewTopic.style.zIndex = '0';
     }
 }
 
 function hiddenNavbar() {
-    navbar.style.left = "-100%";
+    navbar.style.left = '-100%';
     backgroundBlack.style.display = 'none';
 }
